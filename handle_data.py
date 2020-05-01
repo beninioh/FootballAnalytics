87ef0e23dc23df6fec9e9362214110df06217456
beninioh <beninioh>
2020-05-01 17:20:19 +0200

    ENH : Improve handle_data to work with opta format

--- handle_data.py
+++ handle_data.py
@@ -1,6 +1,38 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('understat_data/understat.com.csv')
 
-df_nice = df.query("team == 'Nice'")
+def add_points_and_ranking(df):
+    points = []
+    for i in range(len(df)):
+        wins = df.loc[i, 'wins']
+        draw = 38 - df.loc[i, 'wins'] + df.loc[i, 'losses']
+        points.append(3*wins + draw)
+
+    df['ranking'] = list(range(20)) * 12
+    df['points'] = points
+
+    return df
+
+
+def correlation(df):
+    # sns.heatmap(abs(df.corr()))
+    # plt.show()
+
+    return df.corr().classement
+
+
+def mean(df):
+    return df.mean()
+
+
+df = pd.read_csv('premier_league/stats.csv')
+
+df_correlation = df.groupby('season').apply(correlation)
+df_correlation = df_correlation.mean().sort_values(ascending=False)
+
+df_ranking_mean = df.groupby('classement').apply(mean)
 breakpoint()
+
+
