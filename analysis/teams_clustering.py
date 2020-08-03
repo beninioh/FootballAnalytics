13f241a5d55f3c6065cfc3e83346bc223373f1d6
beninioh <beninioh>
2020-08-04 00:19:19 +0200

    ENH : Normalized team's data before clustering

--- analysis/teams_clustering.py
+++ analysis/teams_clustering.py
@@ -1,51 +1,64 @@
+from utils.constants import TEAM_CLUSTER2
 from utils.build_database import *
-import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
+from typing import List
 import seaborn as sns
 import pandas as pd
-from utils.constants import TEAM_CLUSTER2
-from sklearn.manifold import TSNE
-from sklearn.cluster import KMeans
+import numpy as np
 
 
-def get_team(league, season, db: str = '../rotowire/database/GamesInfo.db'):
+def get_team(league: str, season: str, db: str = '../rotowire/database/GamesInfo.db'):
+    """
+    For a specific league and season, return the teams's stat for all the 20 teams at the end of the season.
+    :param league: str. League's name.
+    :param season: str. Season's name.
+    :param db: str. Path to the database containing the games.
+    :return: pd.DataFrame. Contains the teams values.
+    """
     conn = sqlite3.connect(db)
     games = pd.read_sql_query(f'SELECT * FROM {league}_{season}', con=conn)
-    teams = summarise_teams(games, league, season)
-    # teams.team = teams.team + f'_{league[:4]}_{season[:2]}'
-    # teams_names = teams.team.values
-    # teams = teams.replace([np.inf, -np.inf, np.nan], 0).loc[:, TEAM_CLUSTER2].values
+    teams = summarise_teams(games, league, season, nb_games=True)
 
-    return teams
+    teams_int = teams.select_dtypes(include='float').divide(teams.nb_games, axis=0)
+    teams.drop(columns=teams.select_dtypes(include='float').keys(), inplace=True)
+    teams_normed = pd.concat([teams, teams_int], axis=1, sort=False).drop(columns=['nb_games'])
+
+    return teams_normed
 
 
-# teams = pd.concat([get_team(league, season) for league in ['ligue1', 'liga', 'prleague'] for season in ['1617', '1718', '1819']],
-#                   ignore_index=True, sort=False)
-# teams_names = teams.team.values
-# teams = teams.replace([np.inf, -np.inf, np.nan], 0).loc[:, TEAM_CLUSTER2].values
-#
-# tsne = TSNE(perplexity=4)
-# X_embedded = tsne.fit_transform(teams)
-#
-# for nb_clusters in range(4, 10):
-#     kmeans = KMeans(n_clusters=nb_clusters, random_state=0).fit(X_embedded)
-#
-#     sns.scatterplot(X_embedded[:, 0], X_embedded[:, 1], hue=kmeans.labels_,
-#                     palette=sns.color_palette("husl", len(set(kmeans.labels_))))
-#     # sns.scatterplot(X_embedded[:, 0], X_embedded[:, 1])
-#     plt.title(f'{nb_clusters}')
-#     plt.show()
-#
-# breakpoint()
+def get_teams(leagues: List[str], seasons: List[str], db: str = '../rotowire/database/GamesInfo.db',
+              normalized: bool = True):
+    """
+    For different leagues and seasons, return the team's stat for all teams at the end of the season.
+    :param leagues: List[str]. All the desired league.
+    :param seasons: List. All the desired season.
+    :param db: str. Path to the database containing the games.
+    :param normalized: bool. True if the return data should be normalized.
+    :return: pd.DataFrame. Contains the teams values.
+    """
+    teams = pd.concat([get_team(league, season, db) for league in leagues for season in seasons],
+                      ignore_index=True, sort=False)
+    teams_names = teams.team.values
+    teams = teams.replace([np.inf, -np.inf, np.nan], 0).loc[:, TEAM_CLUSTER2].values
 
-db: str = '../rotowire/database/GamesInfo.db'
-conn = sqlite3.connect(db)
-games = pd.concat([pd.read_sql_query(f'SELECT * FROM {league}_{season}', con=conn)
-                   for league in ['ligue1', 'liga', 'prleague']
-                   for season in ['1617', '1718', '1819']],
-                  ignore_index=True, sort=False)
+    if normalized:
+        teams = normalized_teams(teams)
 
-cluster = pd.read_excel('clustering.xlsx')
+    return teams, teams_names
+
+
+def normalized_teams(teams: List[np.ndarray]):
+    teams_max = [np.nanmax(list(map(lambda x: x[i], teams))) for i in range(len(teams[0]))]
+    teams_min = [np.nanmin(list(map(lambda x: x[i], teams))) for i in range(len(teams[0]))]
+
+    teams_normed = []
+    for team in teams:
+        team_normed = [(team[i] - teams_min[i]) / (teams_max[i] - teams_min[i]) for i in range(len(teams[0]))]
+        teams_normed.append(team_normed)
+
+    return np.array(teams_normed)
 
 
 def extract_score(array, a, b):
@@ -53,8 +66,26 @@ def extract_score(array, a, b):
         return array[2]
 
 
-def get_cluster_tables(games: pd.DataFrame, cluster: pd.DataFrame):
+def get_cluster_tables(leagues: List[str], seasons: List[str], cluster: pd.DataFrame,
+                       db: str = '../rotowire/database/GamesInfo.db'):
+    """
+    From different leagues and seasons, and a specific cluster pre-computed, would return the three cluster tables;
+    corresponding to the interaction between cluster and asserting the likelihood from one team to win/draw/lose
+    against another teams.
+    The table size corresponds to the number of cluster. Ex : Team from cluster 1 playing against team from cluster 3.
+    From the 3 tables, we can see the proportion of a win, a draw or a lose.
+    :param leagues: List[str]. All the desired league.
+    :param seasons: List. All the desired season.
+    :param cluster: pd.DataFrame. Thanks to a pre-computed clustering, this would tell us which team is in which cluster.
+    :param db: str. Path to the database containing the games.
+    :return: List[pd.DataFrame]. Three data frame representing the likelihood of win/draw/lose.
+    """
     cluster_infos = []
+    conn = sqlite3.connect(db)
+    games = pd.concat([pd.read_sql_query(f'SELECT * FROM {league}_{season}', con=conn)
+                       for league in leagues
+                       for season in seasons],
+                      ignore_index=True, sort=False)
     for i in range(len(games)):
         game = games.iloc[i]
         home_cluster = \
@@ -92,7 +123,29 @@ def get_cluster_tables(games: pd.DataFrame, cluster: pd.DataFrame):
     return df_w, df_d, df_l
 
 
-get_cluster_tables(games, cluster)
+teams, _ = get_teams(['ligue1', 'prleague', 'liga'], ['1617', '1718', '1819', '1920'])
+
+for perplex in range(5, 150, 5):
+    tsne = TSNE(perplexity=perplex)
+    X_embedded = tsne.fit_transform(teams)
+    sns.scatterplot(X_embedded[:, 0], X_embedded[:, 1])
+    plt.title(f'{perplex}')
+    plt.show()
+
+# for nb_clusters in range(4, 10):
+#     kmeans = KMeans(n_clusters=nb_clusters, random_state=0).fit(X_embedded)
+#
+#     sns.scatterplot(X_embedded[:, 0], X_embedded[:, 1], hue=kmeans.labels_,
+#                     palette=sns.color_palette("husl", len(set(kmeans.labels_))))
+#     # sns.scatterplot(X_embedded[:, 0], X_embedded[:, 1])
+#     plt.title(f'{nb_clusters}')
+#     plt.show()
+
+breakpoint()
+
+
+cluster2 = pd.read_excel('clustering.xlsx')
+t_win, t_draw, t_lose = get_cluster_tables(['ligue1', 'prleague', 'liga'], ['1617', '1718', '1819'], cluster2)
 
 breakpoint()
 
